e5598660c82ec8df7febe9dbd4f9c8c66893c036
Lewis Dickson <lewisdickson09@gmail.com>
2024-10-24 14:01:01 +0100

    added data formatter

--- DiffusionForecast/DataReformatter.py
+++ DiffusionForecast/DataReformatter.py
@@ -8,18 +8,40 @@ Reformatter for the API call into the standard data format for the competition
 # User Settings 
 #==========================================
 
-data_path = 
+data_path = r'/home/ldickson/AIForecast/Competition Data/Data/openmeteo_last10years.csv' #AQ_varaibles_2yrs.csv
 
 #==========================================
 # Imports 
 #==========================================
 
+import pandas as pd
+import datetime 
+import os 
 
 #==========================================
 # Functions
 #==========================================
 
+def date_format_alteration(dF, dateheader):
+    for idx, date in dF[dateheader].items():
+        # Convert the string to a datetime object
+        date_obj = pd.to_datetime(date)
+        
+        # Reformat the date to the desired format
+        reformatted_date = date_obj.strftime('%Y-%m-%d %H:%M:%S+00:00')
+        
+        # Update the DataFrame with the reformatted date
+        dF.at[idx, dateheader] = reformatted_date
+    
+    return dF
 
 #==========================================
 # Main 
 #==========================================
+
+dF = pd.read_csv(data_path)
+dF_reformatted =date_format_alteration(dF, dateheader='date')
+print(dF_reformatted)
+fname = os.path.basename(data_path)
+
+dF_reformatted.to_csv(f'reformatted_{fname}', index=False)
